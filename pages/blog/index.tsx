812a0f682405ffb0dd646b91d1ffb8b974b4c095
TillJonas28 <till@meyerjark.de>
2023-04-19 20:51:24 +0100

    added dynamic post data from WP

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,27 +5,33 @@ import Link from "next/link"
 
 export async function getServerSideProps() {
     const res = await fetch("https://meyerjark.de/wp-json/wp/v2/categories")
-    const data = await res.json()
-  
+    const categories = await res.json()
+
+    const res2 = await fetch("https://meyerjark.de/wp-json/wp/v2/posts?per_page=20")
+    const posts = await res2.json()
+    
     return {
       props: {
-        data
+        categories,
+        posts
       }
     }
   }
 
-export default function BlogPage({data}:any) {
+export default function BlogPage({categories, posts}:any) {
     const [theme, setTheme] = useState(true)
 
     const handleTheme = () => {
       setTheme(old => !old )
     }
 
-    console.log(data)
+    const RECOMMENDATION = posts.filter( (post:any) => post.id === 4653)
 
-    console.log(data[0].name)
+    console.log(categories[0].name)
 
-    const categoriesRendered = data.map( (category:any) => {
+    console.log(RECOMMENDATION[0])
+
+    const categoriesRendered = categories.map( (category:any) => {
         return (
             <Card key={category.id} title={category.name} desc={category.description} theme={theme} />
         )
@@ -37,7 +43,7 @@ export default function BlogPage({data}:any) {
                 <section className="my-6" id="recommendation-article">
                     <h1 className="text-2xl font-semibold mb-4">Meine Empfehlungen</h1>
                     <div className="grid md:grid-cols-2 gap-7">
-                        <Card title={"Hot Shot Article"} desc={"Lorem ipsum dolor sit amet"} theme={theme}/>
+                        <Card title={RECOMMENDATION[0].title.rendered} desc={RECOMMENDATION[0].excerpt.rendered} theme={theme}/>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer place-self-center w-10 h-10">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
